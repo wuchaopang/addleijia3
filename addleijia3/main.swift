cc2b19f9b727290df12ba1e73f41b33cf880eac3
wuchaopang <20141105049wch@mac23.local>
2016-06-20 11:15:46 +0800

    Initial Commit

---
+++ addleijia3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  addleijia3
+//
+//  Created by 20141105049 on 16/6/20.
+//  Copyright © 2016年 20141105049. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
